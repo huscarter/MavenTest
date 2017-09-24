4721b39fb0a3c71f97d87d926d4cc15761af2c62
weihuihui <weihuihui@shipindiy.com>
2017-09-24 17:44:37 +0800

    Initial commit

---
+++ utillibrary/src/main/java/com/whh/utillibrary/LibraryActivity.java
@@ -0,0 +1,21 @@
+package com.whh.utillibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author huscarter@163.com
+ * @title
+ * @description
+ * @date 9/24/17
+ */
+
+public class LibraryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_library);
+    }
+}
